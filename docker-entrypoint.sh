a06ddfe453baed3833d1697d02f124235b202f9a
Fmelanda <felipeemelanda@gmail.com>
2026-06-19 18:52:48 -0300

    Corrige permissao do volume no entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-mkdir -p /app/data /app/data/media
-
 if [ "$(id -u)" = "0" ]; then
   chown -R node:node /app/data
+  su-exec node mkdir -p /app/data/media
   exec su-exec node "$@"
 fi
 
+mkdir -p /app/data/media
 exec "$@"
